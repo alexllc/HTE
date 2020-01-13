582ac999d5d26d9e57dea308fed8e7977b33b483
alexllc <laulalexandria@outlook.com>
2020-01-13 21:18:39 +0800

    safeguard against too little tx

--- tx_assign_bins_test.R
+++ tx_assign_bins_test.R
@@ -59,7 +59,7 @@ for(gene in perm_genes) {
     for (i in bins){
         print(paste0("quantile: ", i))
         binary_tx = as.numeric(gene_ex > quantile(gene_ex, i) & gene_ex < quantile(gene_ex, i+0.25))
-    print(binary_tx)
+    # print(binary_tx)
         if (length(unique(binary_tx)) == 1 | sum(binary_tx) < length(binary_tx)*0.1) {
                         print("Gene expression distribution too sparse, skipping.")
                         next
