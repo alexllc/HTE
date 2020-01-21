168cc346fb46987d33838c0aeed09e734237df8b
alexllc <laulalexandria@outlook.com>
2020-01-21 15:23:05 +0800

    filter out non-protein coding first, then sample

--- HTE_expression.R
+++ HTE_expression.R
@@ -236,13 +236,13 @@ exp_matrix = dplyr::select(exp_matrix, -intersect_DEG)
 
 # 4. Prepare covariate matrix, whole dataset matrix and a vectoor of treatment types
 tx_vector = colnames(exp_matrix)[colnames(exp_matrix) %in% c("donorId", "TSS", "portion", "plate", "center") == F]
-tx_vector = sample(tx_vector, 4233)
 
 ensembl = useMart("ensembl","hsapiens_gene_ensembl", host = "useast.ensembl.org")
 res = getBM(attributes=c("ensembl_gene_id", "transcript_biotype"), filters = "ensembl_gene_id", values=tx_vector, mart=ensembl)
 # Only keep protein coding transcripts
 res = res[which(res$transcript_biotype=="protein_coding"),] 
 tx_vector = res$ensembl_gene_id
+tx_vector = sample(tx_vector, 4233)
 
 whole_dataset = inner_join(ss_patient, exp_matrix , by = "donorId")
 whole_dataset = dplyr::select(whole_dataset, c("donorId","outcome", "TSS", "portion", "plate", "center", tx_vector))
@@ -266,4 +266,4 @@ write.csv(result[[2]], paste0(output_file, project, '_expression_calibration_res
 write.csv(result[[3]], paste0(output_file, project, '_expression_median_t_test_result.csv'), quote = F, row.names = F)
 write.csv(result[[4]], paste0(output_file, project, '_expression_permutate_testing_result.csv'), quote = F, row.names = F)
 
-}
+#}
