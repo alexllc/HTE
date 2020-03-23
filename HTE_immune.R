42eff0e0e420cf60edb7afbaa188d524dc4907c0
alexllc <laulalexandria@outlook.com>
2020-03-23 12:34:20 +0800

    skipping COADREAD

--- HTE_immune.R
+++ HTE_immune.R
@@ -44,7 +44,9 @@ setwd(paste0(usrwd, "/HTE/wd/immune_HTE"))
 
 cancer_types = c(
     #'ACC','BLCA','BRCA','CESC','CHOL',
-    'COAD','COADREAD','DLBC','ESCA','GBM','GBMLGG','HNSC','KICH','KIPAN','KIRC','KIRP','LGG','LIHC','LUAD','LUSC','MESO','OV','PAAD','PCPG','PRAD','READ','SARC','SKCM','STAD','STES','TGCT','THCA','THYM','UCEC','UCS','UVM')
+    'COAD',
+    #'COADREAD',
+    'DLBC','ESCA','GBM','GBMLGG','HNSC','KICH','KIPAN','KIRC','KIRP','LGG','LIHC','LUAD','LUSC','MESO','OV','PAAD','PCPG','PRAD','READ','SARC','SKCM','STAD','STES','TGCT','THCA','THYM','UCEC','UCS','UVM')
 
     #######################################
     ### CLINICAL DATA
