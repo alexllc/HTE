8719d15be8112b53e67410dc88b7cc66236a0593
alexllc <laulalexandria@outlook.com>
2020-04-02 03:57:40 +0800

    downloading clinical and expression info for all

--- immunotherapy.R
+++ immunotherapy.R
@@ -1,47 +1,68 @@
 library(TCGAbiolinks)
 
-# Fetch survival data from GDC
-query <- GDCquery(project = paste0("TCGA-", project),
+cancer_types = c(
+    'ACC','BLCA','BRCA','CESC','CHOL','COAD','DLBC','ESCA','GBM','HNSC','KICH', 'KIRC','KIRP','LGG','LIHC','LUAD','LUSC','MESO','OV','PAAD','PCPG','PRAD','READ', 'SKCM','STAD', 'TGCT','THCA','THYM','UCEC','UCS','UVM')
+
+## Fixing drug names
+# download.file("https://gdisc.bme.gatech.edu/Data/DrugCorrection.csv", "DrugCorrection.csv")
+# drug_tbl = read.csv("DrugCorrection.csv")
+
+# correct_drug_names <- function(df) {
+#   colnames(drug_tbl)[1] <- "drug_name"
+#   df <- inner_join(df, drug_tbl, by = "drug_name")
+#   df$drug_name <- NULL
+#   colnames(df)[colnames(df)=="Correction"] <- "drug_names"
+#   return(df)
+# }
+
+# correct_drug_cat <- function(cat) {
+#   targeted_rx <- read.csv(paste0(usrwd, "/cancerl/data/targeted_rx.csv"))
+#   targeted_rx[,1] <- sapply(sapply(as.character(targeted_rx[,1]), function(x) strsplit(x, ' ')), function(x) x[[1]])
+#   colnames(targeted_rx)[1] <- "drug_name"
+
+# }
+usrwd = "/exeh_4/alex_lau/"
+setwd(paste0(usrwd, "/HTE/wd/expression_HTE"))
+
+for (project in cancer_types) {
+    print(paste0(paste0(rep('#', 20), collapse=""), "Downloading: ", project, paste0(rep('#', 20), collapse="")))
+    #############################################################
+    ### 1. Prepre TCGA clinical data
+    #############################################################
+
+    # Fetch survival data from GDC
+    query <- GDCquery(project = paste0("TCGA-", project),
                 data.category = "Clinical",
                 file.type = "xml") # Obtain list from TCGAbiolinks:::getProjectSummary(<enter_project>)
 
-#GDCdownload(query)
-load(paste0("./clinical/", project, "_clinical.rda"))
-# patient = GDCprepare_clinic(query, clinical.info = "patient")
+    GDCdownload(query)
+    # clinical_set <- c("drug", "patient")
 
-#save(patient, file = paste0("./clinical/", project, "_clinical.rda"))
-s.patient <-  c("bcr_patient_barcode", "gender","vital_status","days_to_birth", "days_to_death", "days_to_last_followup","race_list", "stage_event_pathologic_stage")
+    # for (c in clinical_set) {
+    # assign(c, GDCprepare_clinic(query, clinical.info = c))
+    # }
 
-s.df <- NA
-for (i in s.patient) {
-    s.df <- cbind(s.df, patient[grep(i, colnames(patient))])
-}
-s.df <- s.df[,-1]
-df_patient <- s.df[!duplicated(s.df),]
+    ### Fix drug names
 
-# Process patient info
-df_patient$vital_status <- sapply(as.numeric(df_patient$vital_status), function(x) x - 1)
-df_patient <- df_patient %>% mutate(survival_time = coalesce(days_to_death, days_to_last_followup)) # there are some negative days
-max.censored <- max(df_patient$survival_time[df_patient$vital_status == 0])
-df_patient <- df_patient[df_patient$survival_time >= 0,]
-df_patient$vital_status[df_patient$survival_time == max.censored] <- 1
-df_patient$imputed.log.times <- impute.survival(df_patient$survival_time, df_patient$vital_status)
-df_patient$age <- floor(df_patient$days_to_birth/365)/-1# Convert age
 
-extra <- c("days_to_birth", "survival_time", "days_to_last_followup", "days_to_death")
-df_patient <- dplyr::select(df_patient, -extra)
-df_patient <- df_patient[df_patient$imputed.log.times >= 0,]
+    #############################################################
+    ### 2. Prepare 
+    #############################################################
 
-df_patient[df_patient==""] <- NA
+    if (!file.exists(paste0("./HTSeqData/", project, "_exp.rda")) ) {
 
-for (c in colnames(df_patient)) {
+    # Fetch expression data from GDC
+    g_query <- GDCquery(project = paste0("TCGA-", project),
+                    data.category = "Transcriptome Profiling",
+                    legacy = F,
+                    data.type = "Gene Expression Quantification",
+                    workflow.type = "HTSeq - FPKM-UQ",
+                    sample.type = "Primary solid Tumor")
 
-    if (!is.numeric(df_patient[,c]) && c != "bcr_patient_barcode") {
-        which.one <- which( levels(df_patient[,c]) == "")
-        levels(df_patient[,c])[which.one] <- NA
-        df_patient[,c] = sapply(sapply(df_patient[,c], as.factor), as.numeric) 
-        print(paste0(c, " is altered")) 
+    GDCdownload(g_query)
+    expdat <- GDCprepare(query = g_query,
+                        save = TRUE,
+                        save.filename = paste0("./HTSeqData/",project, "_exp.rda"))
     }
-}
 
-df_patient = na.omit(df_patient)
+}
