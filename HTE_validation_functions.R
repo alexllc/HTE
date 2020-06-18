89c7487a95a7b58860a2841db9ce798dd5c508fc
alexllc <laulalexandria@outlook.com>
2020-06-18 22:14:16 +0800

    update HTE mut file for varimp validation

--- HTE_validation_functions.R
+++ HTE_validation_functions.R
@@ -180,11 +180,11 @@ split_half_testing <- function(covariates, Y,
             write.csv(observation_result.b, file = paste0(file_prefix,  '_observation_', i,'_result_b.csv'))
             
             # Extract varimp from each of the split half forest (Jun 13, 2020 @alex)
-            varImp = variable_importance(tau.forest.train)
-            train.varimp <- data.frame(variable = varimp_names, varImp, max.depth = 4)
+            varImp = variable_importance(tau.forest.train, max.depth = 4)
+            train.varimp <- data.frame(variable = varimp_names, varImp)
             write.csv(train.varimp, file = paste0(file_prefix, '_observation_', i, '_varimp_train.csv'), row.names = F, quote = F)
 
-            varImp = variable_importance(tau.forest.test)
+            varImp = variable_importance(tau.forest.test, max.depth = 4)
             test.varimp <- data.frame(variable = varimp_names, varImp, max.depth = 4)
             write.csv(test.varimp, file = paste0(file_prefix, '_observation_', i, '_varimp_test.csv'), row.names = F, quote = F)
             #message(paste0("Varimp for observation ", i, " saved."))
